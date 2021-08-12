593b981ad3ecf2a4c512252aaae73886e56884db
MasudiMutheu <88836616+MasudiMutheu@users.noreply.github.com>
2021-08-12 16:06:31 +0300

    Add files via upload

---
+++ My FirstJava/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+       Scanner input =  new Scanner(System.in);
+       System.out.println("Enter number A: ");
+       int number1 = input.nextInt();
+       System.out.println("Enter number B: ");
+       int number2 = input.nextInt();
+       System.out.println(number1+number2);
+       // float dec = input.nextFloat();
+       // double num = input.nextDouble();
+
+    }
+}
